21b8c901d4b11a81119a7af0cb4f68dde4280a24
commotum <jake@vuca.ai>
2025-04-12 10:51:34 -0700

    .

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,20 @@
 #include <iostream>
 
 int main() {
-    // Create layer: 4 inputs, 2 outputs
-    QuaternionDenseLayer layer(4, 2);
-
-    // Allocate input/output on GPU
+    QuaternionDenseLayer layer(1024, 512);
     Quaternion *d_input, *d_output;
-    cudaMalloc(&d_input, 4 * sizeof(Quaternion));
-    cudaMalloc(&d_output, 2 * sizeof(Quaternion));
-
-    // Host input example: all 1's
-    Quaternion h_input[4] = {{1,1,1,1}, {1,1,1,1}, {1,1,1,1}, {1,1,1,1}};
-    cudaMemcpy(d_input, h_input, 4 * sizeof(Quaternion), cudaMemcpyHostToDevice);
-
-    // Forward pass
-    layer.forward(d_input, d_output);
-
-    // Fetch results back to host
-    Quaternion h_output[2];
-    cudaMemcpy(h_output, d_output, 2 * sizeof(Quaternion), cudaMemcpyDeviceToHost);
-
-    // Print results
+    cudaMalloc(&d_input, 1024 * sizeof(Quaternion));
+    cudaMalloc(&d_output, 512 * sizeof(Quaternion));
+    Quaternion h_input[1024];
+    for (int i = 0; i < 1024; i++) {
+        h_input[i] = {1, 1, 1, 1};
+    }
+    cudaMemcpy(d_input, h_input, 1024 * sizeof(Quaternion), cudaMemcpyHostToDevice);
+    for (int i = 0; i < 10000; i++) {
+        layer.forward(d_input, d_output);
+    }
+    Quaternion h_output[512];
+    cudaMemcpy(h_output, d_output, 512 * sizeof(Quaternion), cudaMemcpyDeviceToHost);
     for (int i = 0; i < 2; i++) {
         std::cout << "Output " << i << ": "
                   << h_output[i].w << ", "
@@ -29,7 +23,6 @@ int main() {
                   << h_output[i].y << ", "
                   << h_output[i].z << std::endl;
     }
-
     cudaFree(d_input);
     cudaFree(d_output);
     return 0;
