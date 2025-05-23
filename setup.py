69502282d3d58b5ebe8c1ab0068e6f9140b1422b
commotum <jake@vuca.ai>
2025-05-23 02:05:24 -0700

    .

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 setup(
-    name='quatnet',
+    name='quatnet_cuda',
     ext_modules=[
         CUDAExtension('quatnet_cuda', [
             'src/isokawa_layer.cu',
             'src/quat_ops.cu',
-            'src/hamprod_kernel.cu'
+            'src/hamprod_kernel.cu',
         ])
     ],
-    cmdclass={'build_ext': BuildExtension}
-) 
+    cmdclass={
+        'build_ext': BuildExtension
+    }) 
